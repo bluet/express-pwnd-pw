4b08b08ff1bfeddb2cdee73de10feedec4fbd8b6
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2019-07-16 04:26:43 +0800

    fix test

--- t/5-middleware.test.js
+++ t/5-middleware.test.js
@@ -17,13 +17,8 @@ test('pwnd-pw testing password 12345 should have matches', async (assert) => {
 		assert.true(req.pwndPw);
 		assert.true(req.pwndPw['8cb2237d0679ca88db6464eac60da96345513964']);
 		assert.true(parseInt(req.pwndPw['8cb2237d0679ca88db6464eac60da96345513964']) >= 2333232);
-		assert.deepEqual(
-			req.pwndPw,
-			{
-				'8cb2237d0679ca88db6464eac60da96345513964': 2333232,
-				'230991abcd77e8173edb0af392e1f11120051e29': 6638
-			}
-		);
+		assert.true(req.pwndPw['230991abcd77e8173edb0af392e1f11120051e29']);
+		assert.true(parseInt(req.pwndPw['230991abcd77e8173edb0af392e1f11120051e29']) >= 6638);
 		Promise.resolve(req.pwndPw);
 	});
 	assert.end();
